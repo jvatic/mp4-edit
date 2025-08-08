d6e95ca3f95947cbeda0f9ef11e10ce8d16c5097
Jesse Stuart <email@jessestuart.ca>
2025-08-07 21:32:04 -0400

    chore: export FourCC at crate level

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 pub mod atom;
 pub mod chunk_offset_builder;
 pub mod parser;
-pub mod sample_size_prediction;
 pub mod writer;
 
-pub use atom::{Atom, AtomData};
+pub use atom::{util::FourCC, Atom, AtomData};
 pub use parser::{ParseError, Parser};
 pub use writer::Mp4Writer;
