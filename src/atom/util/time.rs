2e9ba0274edcf5bf43dc647ae1bfa2c2f2529a1a
Jesse Stuart <email@jessestuart.ca>
2025-08-08 14:55:50 -0400

    WIP

--- src/atom/util/time.rs
+++ src/atom/util/time.rs
@@ -1,5 +1,12 @@
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
+pub fn scaled_duration(duration: Duration, timescale: u64) -> u64 {
+    const NANOS_PER_SECOND: u128 = 1_000_000_000;
+    let duration_nanos = duration.as_nanos();
+    let timescale = timescale as u128;
+    (duration_nanos * timescale / NANOS_PER_SECOND) as u64
+}
+
 pub fn mp4_timestamp(duration: Duration) -> u64 {
     duration.as_secs() + 2_082_844_800
 }
