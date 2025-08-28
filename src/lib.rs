0668d016e69e64881bf630f5e9bae72d94f51c09
Jesse Stuart <email@jessestuart.ca>
2025-08-27 21:30:06 -0400

    chore: impl Parse for AtomData

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,6 @@ pub mod chunk_offset_builder;
 pub mod parser;
 pub mod writer;
 
-pub use atom::{util::FourCC, Atom, AtomData};
+pub use atom::{Atom, AtomData, FourCC};
 pub use parser::{ParseError, Parser};
 pub use writer::Mp4Writer;
