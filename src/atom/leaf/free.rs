1fef926bdb96ccf066888484341f9cbb7ff30bd0
Jesse Stuart <email@jessestuart.ca>
2025-09-21 18:20:28 -0400

    WIP(free): minor refactor

--- src/atom/leaf/free.rs
+++ src/atom/leaf/free.rs
@@ -1,10 +1,12 @@
 use futures_io::AsyncRead;
-use futures_util::AsyncReadExt;
 use std::fmt;
 
 use crate::{
-    atom::{util::DebugEllipsis, FourCC},
-    parser::{ParseAtom, ParseErrorKind},
+    atom::{
+        util::{read_to_end, DebugEllipsis},
+        FourCC,
+    },
+    parser::ParseAtom,
     writer::SerializeAtom,
     ParseError,
 };
@@ -35,22 +37,13 @@ impl fmt::Debug for FreeAtom {
 impl ParseAtom for FreeAtom {
     async fn parse<R: AsyncRead + Unpin + Send>(
         atom_type: FourCC,
-        mut reader: R,
+        reader: R,
     ) -> Result<Self, ParseError> {
-        // Verify this is a free or skip atom
         if atom_type != FREE {
             return Err(ParseError::new_unexpected_atom(atom_type, FREE));
         }
 
-        let mut data = Vec::new();
-        reader
-            .read_to_end(&mut data)
-            .await
-            .map_err(|err| ParseError {
-                kind: ParseErrorKind::Io,
-                location: None,
-                source: Some(Box::new(err)),
-            })?;
+        let data = read_to_end(reader).await?;
         Ok(FreeAtom {
             atom_type,
             data_size: data.len(),
@@ -65,7 +58,6 @@ impl SerializeAtom for FreeAtom {
     }
 
     fn into_body_bytes(self) -> Vec<u8> {
-        // Free atoms typically contain zero bytes or preserved data
         if self.data.is_empty() {
             vec![0u8; self.data_size]
         } else {
